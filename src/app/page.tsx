6c2526ab09c7f61f0e23cde162b1de6ea1554057
Tushar Banik <b.toshar@iitg.ac.in>
2024-03-06 00:04:58 +0530

    Input form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import { DataTable } from "@/components/data-table";
+import { Tab } from "@/components/tab";
 
 const Home = () => {
   return ( 
-    <div className="home">
-      <h2>Home Page</h2>
-      <p>Welcome to the home page</p>
+    <div className="home py-16 h-screen w-screen flex flex-col gap-20 items-center">
+      <Tab/>
       <DataTable />
     </div>
    );
