4a236b82d3c9571c3f7177b08d2e7a525ace78c8
Tushar Banik <b.toshar@iitg.ac.in>
2024-03-05 23:56:00 +0530

    data table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import { DataTable } from "@/components/data-table";
+
 const Home = () => {
   return ( 
     <div className="home">
       <h2>Home Page</h2>
       <p>Welcome to the home page</p>
+      <DataTable />
     </div>
    );
 }
