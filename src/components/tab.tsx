787ca90340dc45a957cd057e26b30b9a73701daa
Tushar Banik <b.toshar@iitg.ac.in>
2024-03-06 00:25:00 +0530

    changes

--- src/components/tab.tsx
+++ src/components/tab.tsx
@@ -28,21 +28,17 @@ export const Tab = () => {
           <CardHeader>
             <CardTitle>Account</CardTitle>
             <CardDescription>
-              Make changes to your account here. Click save when you are done.
+              Make changes to your account here. Click Generate when you are done.
             </CardDescription>
           </CardHeader>
           <CardContent className="space-y-2">
             <div className="space-y-1">
-              <Label htmlFor="name">Name</Label>
-              <Input id="name" defaultValue="Pedro Duarte" />
-            </div>
-            <div className="space-y-1">
-              <Label htmlFor="username">Username</Label>
-              <Input id="username" defaultValue="@peduarte" />
+              <Label htmlFor="name">Amino Acids(to be excluded)</Label>
+              <Input id="name" defaultValue="lys,arg..."/>
             </div>
           </CardContent>
           <CardFooter>
-            <Button>Save changes</Button>
+            <Button>Generate</Button>
           </CardFooter>
         </Card>
       </TabsContent>
