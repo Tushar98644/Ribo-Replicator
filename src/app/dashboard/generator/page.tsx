76339323636272205090b30dbcb63ee78728589f
Tushar Banik <b.toshar@iitg.ac.in>
2024-03-18 11:40:34 +0530

    minor changes

---
+++ src/app/dashboard/generator/page.tsx
@@ -0,0 +1,11 @@
+import { Tab } from "@/components/tab";
+
+const Home = () => {
+  return ( 
+    <div className="home py-16 h-screen w-screen flex flex-col gap-20 items-center">
+      <Tab/>
+    </div>
+   );
+}
+ 
+export default Home;
