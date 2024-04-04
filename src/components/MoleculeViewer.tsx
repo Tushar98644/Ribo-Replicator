a5bb201c1bae796355bad37464c56edae0ab4283
Tushar Banik <b.toshar@iitg.ac.in>
2024-04-04 12:44:00 +0530

    useEffect triggered twice fix

--- src/components/MoleculeViewer.tsx
+++ src/components/MoleculeViewer.tsx
@@ -4,7 +4,7 @@ import { useEffect } from 'react';
 
 
 async function getRandomAvailablePDBCode() {
-  const availablePDBCodes = ['1AIY', '1BEN', '1EV3', '1EV6', '1EVR', '1FU2','1FUB','1G7A','1G7B','1GUJ','1J73','1JCA','1MSO','1OS3','1SF1','1TRZ','1TYL'];
+  const availablePDBCodes = ['1AIY', '1BEN', '1EV3', '1EV6', '1EVR', '1FU2', '1FUB', '1G7A', '1G7B', '1GUJ', '1J73', '1JCA', '1MSO', '1OS3', '1SF1', '1TRZ', '1TYL'];
 
   const randomIndex = Math.floor(Math.random() * availablePDBCodes.length);
   console.log(`Random PDB code: ${availablePDBCodes[randomIndex]}`);
@@ -13,6 +13,9 @@ async function getRandomAvailablePDBCode() {
 
 export const MoleculeViewer = () => {
   useEffect(() => {
+    console.log('useEffect triggered');
+
+    let isMounted = true;
     const loadScripts = async () => {
       await new Promise((resolve, reject) => {
         const script1 = document.createElement('script');
@@ -32,42 +35,48 @@ export const MoleculeViewer = () => {
     };
 
     loadScripts().then(async () => {
-      // @ts-ignore
-      if (window?.$3Dmol) {
-        // @ts-ignore
-        window?.$3Dmol.autoload();
-        // @ts-ignore
-        const viewer = $3Dmol.createViewer('viewer-container', {
-          backgroundColor: 'white',
-        });
 
-        const pdbCode = await getRandomAvailablePDBCode();
-        if (!pdbCode) {
-          console.error('No available PDB codes found.');
-          return;
+      if (isMounted) {
+        // @ts-ignore
+        if (window?.$3Dmol) {
+          // @ts-ignore
+          window?.$3Dmol.autoload();
+          // @ts-ignore
+          const viewer = $3Dmol.createViewer('viewer-container', {
+            backgroundColor: 'white',
+          });
+
+          const pdbCode = await getRandomAvailablePDBCode();
+          if (!pdbCode) {
+            console.error('No available PDB codes found.');
+            return;
+          }
+
+          const pdbUri = `https://files.rcsb.org/download/${pdbCode}.pdb`;
+
+          await axios.get(pdbUri)
+            .then(response => {
+              if (response.status !== 200) {
+                throw new Error(`Failed to load PDB: ${response.statusText}`);
+              }
+              return response.data;
+            })
+            .then(data => {
+              viewer.addModel(data, 'pdb');
+              viewer.setStyle({}, { cartoon: { color: 'spectrum' } });
+              viewer.zoomTo();
+              viewer.render();
+              viewer.zoom(1.2, 1000);
+            })
+            .catch(error => console.log(`Failed to load PDB ${pdbUri}: ${error}`));
         }
-
-        const pdbUri = `https://files.rcsb.org/download/${pdbCode}.pdb`;
-
-        await axios.get(pdbUri)
-          .then(response => {
-            if (response.status !== 200) {
-              throw new Error(`Failed to load PDB: ${response.statusText}`);
-            }
-            return response.data;
-          })
-          .then(data => {
-            viewer.addModel(data, 'pdb');
-            viewer.setStyle({}, { cartoon: { color: 'spectrum' } });
-            viewer.zoomTo();
-            viewer.render();
-            viewer.zoom(1.2, 1000);
-          })
-          .catch(error => console.log(`Failed to load PDB ${pdbUri}: ${error}`));
       }
+      // @ts-ignore
+
     });
 
     return () => {
+      isMounted = false;
       const script1 = document.querySelector('script[src="https://3Dmol.org/build/3Dmol-min.js"]');
       const script2 = document.querySelector('script[src="https://3Dmol.org/build/3Dmol.ui-min.js"]');
       script1 && script1.remove();
